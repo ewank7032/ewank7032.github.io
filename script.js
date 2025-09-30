59589306f11895248a542d564a1e2eaa30dedbe3
ewank7032 <yuhangewank831@proton.me>
2025-09-30 09:21:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Yu Hang Ewan Kong"; // update the part BETWEEN the "quotes"
+let userName = "ewank7032"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
